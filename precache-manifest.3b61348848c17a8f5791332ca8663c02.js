6674466adbff9b9c64469e304c55374f9bcf5636
sliwey <qlw1009@gmail.com>
2019-02-17 18:56:53 +0800

    Updates

---
+++ precache-manifest.3b61348848c17a8f5791332ca8663c02.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0d207774c0d975a1e115a2ac3f53774",
+    "url": "/toast/static/media/loading.e0d20777.gif"
+  },
+  {
+    "revision": "94bb605f23e838fa840a",
+    "url": "/toast/static/js/runtime~main.94bb605f.js"
+  },
+  {
+    "revision": "a3a9c6e508a65ceb5077",
+    "url": "/toast/static/js/main.a3a9c6e5.chunk.js"
+  },
+  {
+    "revision": "e6b63711c66cce498eb2",
+    "url": "/toast/static/js/2.e6b63711.chunk.js"
+  },
+  {
+    "revision": "a3a9c6e508a65ceb5077",
+    "url": "/toast/static/css/main.ecbf4522.chunk.css"
+  },
+  {
+    "revision": "2c1c01b51047c2ed005547ade9bf2c7d",
+    "url": "/toast/index.html"
+  }
+];
